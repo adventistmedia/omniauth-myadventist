5162d40d892eecb6fce3c4e8e6a07351f0908c54
danlewis <flightofdan@gmail.com>
2016-11-14 14:15:05 +1100

    experimenting changes

--- lib/omniauth/strategies/myadventist.rb
+++ lib/omniauth/strategies/myadventist.rb
@@ -11,7 +11,31 @@ module OmniAuth
         token_url: 'https://test.myadventist.org.au/oauth/gettoken'
       }
 
-      uid { access_token.params[:user_id] }
+      uid { raw_info['uid'] }
+
+      info do
+        {
+          'email' => raw_info['PrimaryEmail'],
+          'first_name' => raw_info['FirstName'],
+          'last_name' => raw_info['LastName']
+        }
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get('https://test.myadventist.org.au/oauth/userinfo').parsed
+      end
+
+
+      def callback_url
+        full_host + script_name + callback_path
+      end
+
+      # def build_access_token
+      #   verifier = request.params["code"]
+      #   # myAdventist doesn't accept params on redirect_uri which omniauth adds.
+      #   client.auth_code.get_token(verifier, {"redirect_uri" => callback_url, "state" => request.params["state"]}.merge(token_params.to_hash(:symbolize_keys => true)), deep_symbolize(options.auth_token_params))
+      # end
+
     end
   end
 end
