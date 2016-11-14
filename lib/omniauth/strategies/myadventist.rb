57cb0afdbcbe64bfeb7cc927f23afd61169bed68
danlewis <flightofdan@gmail.com>
2016-11-15 07:54:48 +1100

    changes

--- lib/omniauth/strategies/myadventist.rb
+++ lib/omniauth/strategies/myadventist.rb
@@ -1,4 +1,5 @@
-require 'omniauth-oauth2'
+require 'omniauth/strategies/oauth2'
+require 'uri'
 
 module OmniAuth
   module Strategies
@@ -25,16 +26,35 @@ module OmniAuth
         @raw_info ||= access_token.get('https://test.myadventist.org.au/oauth/userinfo').parsed
       end
 
+      # def callback_phase # rubocop:disable AbcSize, CyclomaticComplexity, MethodLength, PerceivedComplexity
+      #   error = request.params["error_reason"] || request.params["error"]
+      #   if error
+      #     fail!(error, CallbackError.new(request.params["error"], request.params["error_description"] || request.params["error_reason"], request.params["error_uri"]))
+      #   elsif !options.provider_ignores_state && (request.params["state"].to_s.empty? || request.params["state"] != session.delete("omniauth.state"))
+      #     fail!(:csrf_detected, CallbackError.new(:csrf_detected, "CSRF detected"))
+      #   else
+      #     self.access_token = build_access_token
+      #     self.access_token = access_token.refresh! if access_token.expired?
+      #     super
+      #   end
+      #   rescue ::OAuth2::Error, CallbackError => e
+      #     fail!(:invalid_credentials, e)
+      #   rescue ::Timeout::Error, ::Errno::ETIMEDOUT => e
+      #     fail!(:timeout, e)
+      #   rescue ::SocketError => e
+      #     fail!(:failed_to_connect, e)
+      # end
+
 
       def callback_url
-        full_host + script_name + callback_path
+        options[:callback_url] || (full_host + script_name + callback_path)
       end
 
-      # def build_access_token
-      #   verifier = request.params["code"]
-      #   # myAdventist doesn't accept params on redirect_uri which omniauth adds.
-      #   client.auth_code.get_token(verifier, {"redirect_uri" => callback_url, "state" => request.params["state"]}.merge(token_params.to_hash(:symbolize_keys => true)), deep_symbolize(options.auth_token_params))
-      # end
+      def build_access_token
+        verifier = request.params["code"]
+        # myAdventist doesn't accept params on redirect_uri which omniauth adds.
+        client.auth_code.get_token(verifier, {"redirect_uri" => callback_url, "state" => request.params["state"]}.merge(token_params.to_hash(:symbolize_keys => true)), deep_symbolize(options.auth_token_params))
+      end
 
     end
   end
